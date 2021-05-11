2f2419c861d50d3d9100b3c472c2be9be734dc7d
Asmi Rao <seldonbeeblebrox42@gmail.com>
2021-05-11 12:20:47 +0530

    Project 78

--- Screens/loginScreen.js
+++ Screens/loginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground, Modal, KeyboardAvoidingView, ScrollView } from 'react-native';
 import db from '../config';
 import firebase from 'firebase';
 
@@ -10,7 +10,13 @@ export default class LoginScreen extends React.Component {
 
         this.state = {
             emailId: '',
-            password: ''
+            password: '',
+            isModalVisible: false,
+            firstName: '',
+            lastName: '',
+            phoneNumber: '',
+            address: '',
+            confirmPassword: ''
         }
     }
 
@@ -30,18 +36,164 @@ export default class LoginScreen extends React.Component {
     }
 
 
-    signUp = async (emailId, password)=>{
+    signUp = async (emailId, password, confirmPassword)=>{
 
-        firebase.auth().createUserWithEmailAndPassword(emailId, password)
-        .then((response)=>{
-            return alert("User Account created");
-        })
-        .catch((error)=>{
-            var errorCode = error.message;
-            var errorMessage = error.message;
-            return alert(errorMessage);
-        })
+        if(password !== confirmPassword){
+            return alert("Password does not match Confirm Password");
+        }
+        else{
 
+            firebase.auth().createUserWithEmailAndPassword(emailId, password)
+            .then((response)=>{
+              return alert("User Account created");
+            })
+            .catch((error)=>{
+              var errorCode = error.message;
+              var errorMessage = error.message;
+              return alert(errorMessage);
+            });
+
+            db.collection("users").add({
+                first_name: this.state.firstName,
+                last_name: this.state.lastName,
+                phone_number: this.state.phoneNumber,
+                address: this.state.address,
+                emailId: this.state.emailId,
+                password: this.state.password,
+                confirm_password: this.state.confirmPassword
+            });
+
+            return(
+                alert(
+                    'User Registered Successfully',
+                    '',
+                    [{text: 'okay', onPress: ()=> this.hideModal() }]
+                )
+            );
+
+        }
+
+    }
+
+
+    showModal = () =>{
+        return(
+            <Modal
+              animationType = "fade"
+              transparent = {true}
+              visible = {this.state.isModalVisible}
+              style = {[styles.modalView, {marginTop: 80}]}>
+
+                <View style = {styles.modalView}>
+
+                    <ScrollView style = {{width: '100%'}}>
+                        <KeyboardAvoidingView style = {styles.keyboardAvoidingView}>
+
+                            <Text style = {styles.formTitle}>Registration Form</Text>
+
+                            <TextInput 
+                              style = {styles.formInput}
+                              placeholder = {"First Name"}
+                              placeholderTextColor = {'#F6C4B2'}
+                              onChangeText = {(text)=>{
+                                  this.setState({
+                                      firstName: text
+                                  })
+                              }}/>
+
+                            <TextInput 
+                              style = {styles.formInput}
+                              placeholder = {"Last Name"}
+                              placeholderTextColor = {'#F6C4B2'}
+                              onChangeText = {(text)=>{
+                                  this.setState({
+                                      lastName: text
+                                  })
+                              }}/>
+
+                            <TextInput 
+                              style = {styles.formInput}
+                              placeholder = {"Phone Number"}
+                              placeholderTextColor = {'#F6C4B2'}
+                              maxLength = {10}
+                              onChangeText = {(text)=>{
+                                  this.setState({
+                                      phoneNumber: text
+                                  })
+                              }}/>
+
+                            <TextInput 
+                              style = {styles.formInput}
+                              placeholder = {"Residential Address"}
+                              placeholderTextColor = {'#F6C4B2'}
+                              onChangeText = {(text)=>{
+                                  this.setState({
+                                      address: text
+                                  })
+                              }}/>
+
+                            <TextInput 
+                              style = {styles.formInput}
+                              placeholder = {"Email ID"}
+                              placeholderTextColor = {'#F6C4B2'}
+                              keyboardType = 'email-address'
+                              onChangeText = {(text)=>{
+                                  this.setState({
+                                      emailId: text
+                                  })
+                              }}/>
+
+                            <TextInput 
+                              style = {styles.formInput}
+                              placeholder = {"Password"}
+                              placeholderTextColor = {'#F6C4B2'}
+                              secureTextEntry = {true}
+                              onChangeText = {(text)=>{
+                                  this.setState({
+                                      password: text
+                                  })
+                              }}/>
+
+                            <TextInput 
+                              style = {styles.formInput}
+                              placeholder = {"Confirm Password"}
+                              placeholderTextColor = {'#F6C4B2'}
+                              secureTextEntry = {true}
+                              onChangeText = {(text)=>{
+                                  this.setState({
+                                      confirmPassword: text
+                                  })
+                              }}/>
+
+                            <TouchableOpacity
+                              style = {[styles.registerButton, {marginTop: 10}]}
+                              onPress = {()=>{
+                                  this.signUp(this.state.emailId, this.state.password, this.state.confirmPassword);
+                              }}>
+                                <Text style = {styles.buttonText}>REGISTER</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity
+                              style = {[styles.registerButton, {marginTop: 13}]}
+                              onPress = {()=>{
+                                  this.hideModal();
+                              }}>
+                                <Text style = {[styles.buttonText]}>CANCEL</Text>
+                            </TouchableOpacity>
+
+                        </KeyboardAvoidingView>
+                    </ScrollView>
+
+                </View>
+            </Modal>
+        )
+    }
+
+
+    hideModal = ()=>{
+        this.setState({
+            isModalVisible: false
+        });
     }
 
 
@@ -54,7 +206,11 @@ export default class LoginScreen extends React.Component {
                 <View style = {styles.bgBlur} />
                 <Text style = {styles.header}>BARTER SYSTEM</Text>
 
-                <View style = {{flex: 1, flexDirection: 'column', marginTop: 90}}>
+                <View>
+                    {this.showModal()}
+                </View>
+
+                <View style = {{flex: 1, flexDirection: 'column', marginTop: 30}}>
                     <TextInput 
                       style = {styles.loginInput}
                       placeholder = "Enter Email-ID"
@@ -83,15 +239,17 @@ export default class LoginScreen extends React.Component {
                     onPress = {()=>{
                         this.login(this.state.emailId, this.state.password);
                     }}>
-                        <Text style = {styles.loginButtonText}>LOGIN</Text>
+                        <Text style = {styles.buttonText}>LOGIN</Text>
                   </TouchableOpacity>
 
                   <TouchableOpacity
-                    style = {styles.loginButton}
+                    style = {[styles.loginButton, {marginBottom: 50}]}
                     onPress = {()=>{
-                       this.signUp(this.state.emailId, this.state.password)
+                       this.setState({
+                           isModalVisible: true
+                       });
                     }}>
-                        <Text style = {styles.loginButtonText}>SIGN UP</Text>
+                        <Text style = {styles.buttonText}>SIGN UP</Text>
                   </TouchableOpacity>
                 </View>
 
@@ -113,11 +271,11 @@ const styles = StyleSheet.create({
     },
 
     header: {
-        fontSize: 60,
+        fontSize: 80,
         fontFamily: 'big caslon',
         alignSelf: 'center',
         color: '#fff',
-        marginTop: -100,
+        marginTop: -115,
     },
 
     loginInput: {
@@ -147,23 +305,57 @@ const styles = StyleSheet.create({
         marginLeft: 300
     },
 
-    loginButtonText: {
+    buttonText: {
         textAlign: 'center',
         fontFamily: 'baskerville'
     },
 
     bgBlur: {
         backgroundColor: 'white',
-        width: '55%',
-        height: 130,
+        width: '65%',
+        height: 140,
         alignSelf: 'center',
         opacity: 0.4,
-        marginTop: 50,
+        marginTop: 90,
+    },
+
+    formInput: {
+        width: 300,
+        height: 30,
+        padding: 10,
+        backgroundColor: '#AE8277',
+        color: '#ffffff',
+        borderBottomWidth: 2,
+        borderBottomColor: '#494951',
+        margin: 10
+    },
+
+    registerButton: {
+        alignSelf: 'center',
+        backgroundColor: '#5C96B6',
+        padding: 10,
+        width: 100
+    },
+
+    formTitle: {
+        alignSelf: 'center',
+        color: '#fff',
+        fontFamily: 'big caslon',
+        fontSize: 20,
+        marginBottom: 10
+    },
+
+    modalView: {
+        borderRadius: 4,
+        borderColor: '#2D3134',
+        padding: 2,
+        alignSelf: 'center'
+    },
+
+    keyboardAvoidingView: {
+        borderWidth: 2,
+        borderColor: '#fff',
+        padding: 9
     }
 
 })
-
-/*
- <View style = {[styles.sideView]} />
-                <View style = {[styles.sideView]} />
-*/
